660e6ad1e2069020e6d5c0759adc4b46eb76e8b4
sachin9345 <135344828+sachin9345@users.noreply.github.com>
2024-08-18 22:13:20 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const app = express();
 const errorMiddleware = require('./Middlewares/error');
-//const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
 
 const path = require('path')
 const dotenv = require('dotenv');
 dotenv.config({path:path.join(__dirname,"config/config.env")});
 
 app.use(express.json());
-//app.use(cookieParser());
-//app.use('/uploads', express.static(path.join(__dirname,'uploads') ) )
+app.use(cookieParser());
+
+app.use(cors());
 
 
-//onst cinedatas = require('./routes/cineDatas')
 const auth = require('./routes/auth')
-//const contactus = require('./routes/contact')
 
 
-//app.use('/api/v1/',cinedatas);
+
+
 app.use('/api/v1/',auth);
-//app.use('/api/v1/',contactus);
+
 
 // Serve static files for production build and handle the root URL
 //if (process.env.NODE_ENV === 'production') {
