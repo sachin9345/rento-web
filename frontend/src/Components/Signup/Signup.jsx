05c4ff28cb70bb3cbcc7fbd8a5d9a33d0d189ca4
sachin9345 <135344828+sachin9345@users.noreply.github.com>
2024-08-18 22:16:31 +0530

    Update Signup.jsx

--- frontend/src/Components/Signup/Signup.jsx
+++ frontend/src/Components/Signup/Signup.jsx
@@ -35,7 +35,7 @@ const Signup = () => {
         }
 
         try {
-            const response = await axios.post('https://hackathon-wny1.vercel.app/api/v1/register', {
+            const response = await axios.post('https://hackathon-3-6lb7.onrender.com/api/v1/register', {
                 name,
                 mobile,
                 email,
